9e7857255d784d9d428ab4c603745b258bae5760
Pavlo Savchuk <pavlo.savchuk@lemberg.co.uk>
2017-06-28 18:18:09 +0300

    getPref1Point() added

--- app/src/main/java/ua/com/lviv/fly/touristhelper/data/manager/ProfileManager.java
+++ app/src/main/java/ua/com/lviv/fly/touristhelper/data/manager/ProfileManager.java
@@ -21,29 +21,34 @@ public class ProfileManager {
     }
 
     public String getPref1() {
-        return  pref.getString(context.getString(R.string.key_1),  "");
+        L.e("Pref1 = " + pref.getString(context.getString(R.string.key_1), ""));
+        return pref.getString(context.getString(R.string.key_1), "");
     }
 
-    public int getPref1Point(){
-        int i = Arrays.asList(context.getResources().getStringArray(R.array.values_1)).indexOf(getPref1());
-//        L.e("Point 1 = " + i);
-//        int[] intArray = context.getResources().getIntArray(R.array.options_1);
-//        if(i == -1){
-//            return intArray[1];
-//        }else {
-//            return intArray[i];
-//        }
-
-        return i;
+    public int getPref1Point() {
+        String pref1 = getPref1();
+        int result = 0;
+        switch (pref1) {
+            case "Не знаю":
+                result = -11;
+                break;
+            case "Так":
+                result = -22;
+                break;
+            case "Ні":
+                result = -33;
+                break;
+        }
+        return result;
 
     }
 
 
     public String getPref2() {
-        return  pref.getString(context.getString(R.string.key_2),  "");
+        return pref.getString(context.getString(R.string.key_2), "");
     }
 
-    public int getPref2Point(){
+    public int getPref2Point() {
         int i = Arrays.asList(context.getResources().getStringArray(R.array.values_1)).indexOf(getPref2());
 //        L.e("Point 1 = " + i);
 //        int[] intArray = context.getResources().getIntArray(R.array.options_1);
