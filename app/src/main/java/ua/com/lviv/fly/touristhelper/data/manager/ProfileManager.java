9ffca994fe653a030d61bb4e1c915f0c628dfc88
Pavlo Savchuk <pavlo.savchuk@lemberg.co.uk>
2017-06-29 13:22:15 +0300

    getPref3

--- app/src/main/java/ua/com/lviv/fly/touristhelper/data/manager/ProfileManager.java
+++ app/src/main/java/ua/com/lviv/fly/touristhelper/data/manager/ProfileManager.java
@@ -17,7 +17,6 @@ public class ProfileManager {
     private int index1 = 0;
     private int index2 = 0;
     private int index3 = 0;
-    private int index4 = 0;
 
     public ProfileManager() {
         this.pref = PreferenceManager.getDefaultSharedPreferences(TemplateApplication.getSharedContext());
@@ -25,14 +24,13 @@ public class ProfileManager {
     }
 
     public String getPref1() {
-        L.e("Pref1 = " + pref.getString(context.getString(R.string.key_1), ""));
         return pref.getString(context.getString(R.string.key_1), "");
     }
 
     public int getPref1Point() {
-        String pref1 = getPref1();
+        String pref = getPref1();
         int result = 0;
-        switch (pref1) {
+        switch (pref) {
             case "Не знаю":
                 result = 1;
                 index1 = index1 + result;
@@ -57,16 +55,52 @@ public class ProfileManager {
     }
 
     public int getPref2Point() {
-        int i = Arrays.asList(context.getResources().getStringArray(R.array.values_1)).indexOf(getPref2());
-//        L.e("Point 1 = " + i);
-//        int[] intArray = context.getResources().getIntArray(R.array.options_1);
-//        if(i == -1){
-//            return intArray[1];
-//        }else {
-//            return intArray[i];
-//        }
+        String pref = getPref2();
+        int result = 0;
+        switch (pref) {
+            case "Не знаю":
+                result = 1;
+                index1 = index1 + result;
+                break;
+            case "Так":
+                result = 2;
+                index2 = index2 + result;
+                break;
+            case "Ні":
+                result = 3;
+                index3 = index3 + result;
+                break;
+        }
 
-        return i;
+        return result;
 
     }
+
+    public String getPref3() {
+        return pref.getString(context.getString(R.string.key_3), "");
+    }
+
+    public int getPref3Point() {
+        String pref = getPref3();
+        int result = 0;
+        switch (pref) {
+            case "Не знаю":
+                result = 1;
+                index1 = index1 + result;
+                break;
+            case "Так":
+                result = 2;
+                index2 = index2 + result;
+                break;
+            case "Ні":
+                result = 3;
+                index3 = index3 + result;
+                break;
+        }
+
+        return result;
+
+    }
+
+
 }
